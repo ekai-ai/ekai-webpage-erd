7e231a2a21102229ce1513e4ae67eceb4b54cd52
Azhar Niaz <99729292+azhar-rayn@users.noreply.github.com>
2025-06-03 13:44:20 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
-#import React from 'react'
+import React from 'react';
+// or if you use hooks:
+import React, { useState, useEffect } from 'react';
+
+// your code below
 import { HomeHeroSection } from './components/home/home-hero-section'
 
 function App() {
