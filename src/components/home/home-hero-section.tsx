ad496fa41134410fac382ef0dcdf912575ecb4a5
Azhar Niaz <99729292+azhar-rayn@users.noreply.github.com>
2025-06-03 13:39:19 +0500

    Update home-hero-section.tsx

--- src/components/home/home-hero-section.tsx
+++ src/components/home/home-hero-section.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+#import React, { useEffect, useRef } from 'react'
 import { HashLink } from 'react-router-hash-link'
 import './home-hero-section.less'
 
